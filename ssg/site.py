d33ffa704619aa39ac798629a9c7f625fc763fb2
Chandankumar Shaw <chandankumar.shaw@STLERI.COM>
2022-08-05 00:15:25 +0530

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -3,21 +3,19 @@ from pathlib import Path
 
 from ssg import extensions, hooks
 
+
 class Site:
     def __init__(self, source, dest, parsers=None):
-        self.source = Path(source) 
+        self.source = Path(source)
         self.dest = Path(dest)
         self.parsers = parsers or []
-
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
-
     def load_parser(self, ext):
         for parser in self.parsers:
             if parser.valid_file_ext(ext):
                 return parser
-
     def run_parser(self, path):
         parser = self.load_parser(path.suffix)
         if parser is not None:
@@ -30,12 +28,14 @@ class Site:
     def build(self):
         extensions.load_bundled()
         hooks.event("collect_files", self.source, self.parsers)
+        hooks.event("start_build")
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
                 self.run_parser(path)
+        hooks.event("stats")
 
     @staticmethod
     def error(message):
